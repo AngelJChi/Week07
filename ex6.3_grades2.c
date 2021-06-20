482b12342644bf1414a87e6838cad33b649dddd3
AngelJChi <2009039@upy.edu.mx>
2021-06-20 15:05:57 -0500

    6.3

---
+++ ex6.3_grades2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+
+	int porcentaje_correcto;
+	char calificacion;
+	char modificador;
+	char linea[100];
+
+	printf("Escriba el porcentaje correcto: ");
+	fgets(linea, sizeof(linea), stdin);
+	sscanf(linea, "%d", &porcentaje_correcto);
+
+	modificador = '';
+
+	if (porcentaje_correcto > 100) {
+		printf("Error: fuera de los limites\n");
+		return 1;
+	} else if (porcentaje correcto < 0) {
+		printf("Error: fuera de los limites\n");
+		return 1;
+	} else if (porcentaje_correcto > 91) {
+		calificaion = 'A';
+		if (porcentaje_correcto >= 98) {
+			modificador = '+';
+		} else if (porcentaje_correcto <= 93) {
+			modificador = '-';
+		}
+	} else if (porcentaje_correcto >= 81) {
+		calificacion = 'B';
+		if (porcentaje_correcto >= 88) {
+			modificador = '+';
+		} else if (porcentaje_correcto <=83) {
+			modificador = '-';
+		}
+	} else if (porcecntaje_correcto >= 71) {
+		calififcacion = 'C';
+		if (porcentaje_correcto >= 78) {
+			modificador = '+'
+		} else if (porcentaje_correcto <= 73) {
+			modificador = '-';
+		}
+	} else if (porcentaje_correcto >= 61) {
+		calificacion = 'D';
+		if (pocentaje_correcto >= 68) {
+			modificador = '+';
+		} else if (porcentaje_correcto <= 63) {
+			modificador = '-';
+		}
+	} else {
+		calificacion = 'F';
+	}
+
+	printf("La calificacion es %c%c\n", calificacion, modificador);
+
+	return 0;
+}
