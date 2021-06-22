73cd37cc8ac377ff0ac93e6bec1c475fba8b5421
AngelJChi <2009039@upy.edu.mx>
2021-06-21 20:07:21 -0500

    7.4

---
+++ ex7.4_sales.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#undef DEBUG
+
+
+int main(){
+
+	char liena[100];
+
+	float cantidad;
+	float impuesto;
+
+	float total;
+
+	const float tasa_impuesto = 0.08;
+
+	while (1){
+
+		printf("\nEscriba la cantidad o 0 para salir: ");
+		fgets(linea, sizeof(linea), stdin);
+		sscanf(linea, "%f", &cantidad);
+
+		if (cantidad == 0.0) {
+			printf("Saliendo.\n");
+			return 0;
+		}
+
+		impuesto = cantidad * tasa_impuesto;
+		total = cantidad + impuesto;
+
+		printf("DEBUG:main(); la cantidad es %f\n", cantidad);
+		printf("DEBUG:main(): el impuesto es %f\n", impuesto);
+		printf("DEBUG:main(): el total es %f\n", total);
+
+		printf("El total es %0.2\n", total);
+
+	}
+
+	return 0;
+}
