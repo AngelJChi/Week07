76b48c5f0ae9281e9aa2bb1b4b84c45b9ef48e90
AngelJChi <83776637+AngelJChi@users.noreply.github.com>
2021-06-24 16:37:55 -0500

    Update ex6.1_distance.c

--- ex6.1_distance.c
+++ ex6.1_distance.c
@@ -2,15 +2,15 @@
 
 int main(){
 
-	char linea[100];
+	char line[100];
 	float distancia;
 	float cuadrado;
 	float raiz_cuadrada;
 	
 
 	printf("Escribe la distancia: ");
-	fgets(linea, sizeof(linea), stdin);
-	sscanf(linea, "%f", &distancia);
+	fgets(line, sizeof(line), stdin);
+	sscanf(line, "%f", &distancia);
 
 	cuadrado = distancia * distancia;
 	printf("El cuadrado de la distancia es: %f\n", cuadrado);
